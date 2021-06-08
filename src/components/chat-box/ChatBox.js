ee7962abc3c7967f01f0945caa6236b35bd2d637
Yash Roy <80704137+yashroy80@users.noreply.github.com>
2021-06-09 02:44:02 +0530

    commit-msg

--- src/components/chat-box/ChatBox.js
+++ src/components/chat-box/ChatBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './ChatBox.css'
 import Avatar from '../chat-list/Avatar.js'
 
@@ -8,16 +8,36 @@ export default function ChatBox() {
     const [inputMsg, setInputMsg] = useState();
     const [msgs, setMsgs] = useState(JSON.parse(localStorage.getItem('msgs')))
     //console.log(msgs);
+
+    //Scrolling to bottom of messages
+    const msgEnd = useRef(null);
+    const msgContainer = useRef(null);
+
+    const scrollToBottom = () => {
+        msgEnd.current?.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(() => { 
+        scrollToBottom(); // for initial render of chat-content
+        if (msgContainer) { // for new messages
+            msgContainer.current?.addEventListener('DOMNodeInserted', event => {
+                const { currentTarget: target } = event;
+                target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+            });
+        }
+    })
+
     useEffect(() => {
         const pollFunction = setInterval(() => {
             setCLickedUser(JSON.parse(sessionStorage.getItem('clickedUser')));
             setMsgs(JSON.parse(localStorage.getItem('msgs')));
         }, 500);
-        window.onbeforeunload=()=>{
+        window.onbeforeunload = () => {
             clearInterval(pollFunction);
             return sessionStorage.removeItem('clickedUser');
         }
     })
+
     const enterHandler = (e) => {
         if (e.target.value !== "" && e.key === 'Enter') {
             let newmsgs = JSON.parse(localStorage.getItem('msgs'));
@@ -48,11 +68,11 @@ export default function ChatBox() {
                 <div className="chat-header">
                     <Avatar image={clickedUser.image} />{clickedUser.name}
                 </div>
-                <div className="chat-content">
-                    {msgs.map(mainuser=>{
-                        if(mainuser.id==userProf.id){
-                            return mainuser.msgs.map(inneruser=>{
-                                if(inneruser.id===clickedUser.id){
+                <div className="chat-content" ref={msgContainer}>
+                    {msgs.map(mainuser => {
+                        if (mainuser.id == userProf.id) {
+                            return mainuser.msgs.map(inneruser => {
+                                if (inneruser.id === clickedUser.id) {
                                     return inneruser.msgs.map((item, index) => {
                                         return <div className={item.id === userProf.id ? "current" : "other"} key={index}>{item.msg}</div>
                                     })
@@ -60,6 +80,9 @@ export default function ChatBox() {
                             })
                         }
                     })}
+                    <div style={{ float: "left", clear: "both" }}//dummy div for bottom scroll
+                        ref={msgEnd}>
+                    </div>
                 </div>
                 <div className="chat-footer">
                     <input id="msg-input" value={inputMsg} onChange={(e) => { setInputMsg(e.target.value) }} onKeyPress={enterHandler} type="text" placeholder="Write message"></input>
